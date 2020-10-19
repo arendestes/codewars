a6e04d26650c4f7fba18899f31b5f8566da6cba9
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-10-19 09:00:13 -0500

    completed Password Should Not Contain Any Part Of Your Username kata in js

---
+++ password-should-not-contain-any-part-of-your-username.js
@@ -0,0 +1,39 @@
+// You are writing a password validator for a website. You want to discourage 
+// users from using their username as part of their password, or vice-versa, 
+// because it is insecure. Since it is unreasonable to simply not allow them 
+// to have any letters in common, you come up with this rule:
+
+// For any password and username pair, the length of the longest common substring 
+// should be less than half the length of the shortest of the two.
+
+// In other words, you won't allow users to have half their password repeated 
+// in their username, or half their username repeated in their password.
+
+// Write a function validate(username, password) which returns true if your rule 
+// is followed, false otherwise.
+
+// Assume:
+
+// Both the username and the password may contain uppercase letters, lowercase 
+// letters, numbers, spaces, and the following special/punctation 
+// characters: !@#$%^&*()_+{}:"<>?[];',.
+
+// The username and password will each be less than 100 characters.
+
+function validate(username, password) {
+    const sorted = [username, password].sort((a, b) => a.length - b.length);
+    const short = sorted[0];
+    const long = sorted[1];
+    const halfShort = Math.ceil(short.length/2);
+    for( let i = 0; i < short.length - halfShort +1; i++){
+        const checkValue = short.slice(i, i + halfShort);
+        if( long.includes(checkValue) ){
+            return false;
+        }
+    }
+    return true;
+}
+
+const result = validate("bliginslogsword", "passwds");
+
+console.log(result);
